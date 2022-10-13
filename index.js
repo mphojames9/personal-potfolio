9b7920d016fdd6dfb5d4ead8f58e9fccf5d926bc
mphojames9 <98138271+mphojames9@users.noreply.github.com>
2022-10-13 21:09:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,37 @@ navLink.forEach(navLink => {
         document.body.classList.remove('change');
     })
 })
+
+//Scrolling up button
+let mybutton = document.getElementById("scroolBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+      let clip = document.querySelectorAll(".vid");
+
+
+      clip.forEach(clip => {
+              /* Adding the event listeners on the video to play/pause the video. */
+      clip.addEventListener("mouseover", function (e) {
+        clip.play();
+     })
+
+     /* Applying the mouse out event to pause the video */
+     clip.addEventListener("mouseout", function (e) {
+        clip.pause();
+     })
+      }) 
+
